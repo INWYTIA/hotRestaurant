517c8959538c000373951da9ec989919c4a8d03e
Michael Baska <mikebaska1@gmail.com>
2020-02-22 12:14:40 -0600

    changed some things

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+const Table = require("./table");
+
 var reservations = [];
 var waitList = [];
+
+var object = new Table(document.getElementById("aleph").textContent(), document.getElementById("beth").textContent(), document.getElementById("gimmel").textContent(), document.getElementById("daleth").textContent())
+
+function tablePush() {
+  if(reservations.length < 5) {
+    reservations.push(object);
+  } else {
+    waitList.push(object);
+  }
+}
+
+tablePush();
