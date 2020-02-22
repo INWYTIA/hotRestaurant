b870a8bbc4d64f55159467b606c4ddc7df7a4b9b
DesignerJennifer <54449493+DesignerJennifer@users.noreply.github.com>
2020-02-22 11:22:09 -0600

    no changes made

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-var reservations = [];
-var waitList = [];
+var reservations = [
+    {
+        patronName: "Bob",
+        phone: "913-867-3699",
+        email: "bob@gmail.com",
+        uniqueID: "asdf1234"
+    }
+];
+
+var waitList = [
+    {
+        patronName: "Jill",
+        phone: "913-867-5309",
+        email: "jill@gmail.com",
+        uniqueID: "asdf1235"
+    }
+];
