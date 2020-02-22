08b375ecec1637022dd0d3a83f5dc70d8f7aaa18
DesignerJennifer <54449493+DesignerJennifer@users.noreply.github.com>
2020-02-22 11:57:01 -0600

    updating routing

--- server.js
+++ server.js
@@ -6,6 +6,30 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log("App listening on PORT " + PORT);
 });
+
+// Routes
+// =============================================
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "main.html"));
+});
+
+app.get("/tables", function (req, res) {
+  res.sendFile(path.join(__dirname, "tables.html"));
+});
+
+app.get("/reservation", function (req, res) {
+  res.sendFile(path.join(__dirname, "reservation.html"));
+});
+
+// Displays tables reserved
+app.get("/api/tablesapi", function (req, res) {
+  return res.json(reservations);
+});
+
+app.get("/api/waitlistapi", function (req, res) {
+  return res.json(waitList);
+});
+
